520ca93d037d85d3e7c11f1b9d1faf9b7b6dbc75
Raffaele Carelle <raffaele.carelle@algoritma.it>
2025-11-27 14:47:58 +0100

    Minify `styles.css` for improved performance and reduced file size.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./public/**/*.html",
     "./public/**/*.js",
   ],
+  safelist: [
+    {
+      pattern: /^status-/,
+    },
+    {
+      pattern: /^bg-(red|green|amber|purple|yellow|gray)-(500|600|700|800|900)/,
+    },
+    {
+      pattern: /^text-(red|green|amber|purple|yellow|gray)-(200|300|400)/,
+    },
+    'rotated',
+    'rotate-90'
+  ],
   theme: {
     extend: {},
   },
